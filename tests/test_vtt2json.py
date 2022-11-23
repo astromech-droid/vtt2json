a8898645a20a5c3da2f90e6da84cd8b5cc638236
astromech_droid <n.yamamoto.py@gmail.com>
2022-11-23 15:44:25 +0900

    最低限の変換機能実装完了

---
+++ tests/test_vtt2json.py
@@ -0,0 +1,66 @@
+import os
+import json
+from vtt2json.vtt2json import to_json
+
+SUBTITLES_DIR = os.path.join(os.path.dirname(__file__), "subtitles")
+
+
+def get_file(filename):
+    return os.path.join(SUBTITLES_DIR, filename)
+
+
+def test_ignore_lines():
+    expected_value = {
+        "parsed_lines": [],
+        "ignored_lines": [
+            "WEBVTT\n",
+            "\n",
+            "STYLE\n",
+            "::cue() {\n",
+            "  font-family: Arial, Helvetica, sans-serif;\n",
+            "}\n",
+        ],
+    }
+    parsed_value = to_json(get_file("ignore_lines.vtt"))
+
+    assert parsed_value == json.dumps(expected_value)
+
+
+def test_regular_lines():
+    expected_value = {
+        "parsed_lines": [
+            {
+                "start": "00:00:01.960",
+                "end": "00:00:03.795",
+                "text": "CLAIRE: Kids! Breakfast!",
+                "_raw_text": "CLAIRE: Kids! Breakfast!",
+            }
+        ],
+        "ignored_lines": [],
+    }
+    parsed_value = to_json(get_file("regular_lines.vtt"))
+
+    assert parsed_value == json.dumps(expected_value)
+
+
+def test_multiple_lines():
+    expected_value = {
+        "parsed_lines": [
+            {
+                "start": "00:00:22.231",
+                "end": "00:00:25.526",
+                "text": "And, wow, you're not wearing that outfit.",
+                "_raw_text": "And, wow, you're not wearing that outfit.",
+            },
+            {
+                "start": "00:00:22.231",
+                "end": "00:00:25.526",
+                "text": "And, wow, you're not wearing that outfit.",
+                "_raw_text": "And, wow, you're not wearing that outfit.",
+            },
+        ],
+        "ignored_lines": [],
+    }
+    parsed_value = to_json(get_file("multiple_lines.vtt"))
+
+    assert parsed_value == json.dumps(expected_value)
---
+++ vtt2json/vtt2json.py
@@ -0,0 +1,60 @@
+import json
+import re
+
+
+def _is_ignored_line(vtt_line: str) -> bool:
+    ignore_patterns = [r"^WEBVTT", r"^STYLE$", r"::cue", r"^ ", r"^\}$", r"^\n$"]
+
+    for ip in ignore_patterns:
+        if re.match(ip, vtt_line):
+            return True
+
+    return False
+
+
+def _is_timestamp_line(vtt_line: str) -> bool:
+    timestamp_pattern = r"\d{2}:\d{2}:\d{2}.\d{3}"
+    timestamp_line_pattern = rf"^{timestamp_pattern} --> {timestamp_pattern}"
+
+    if re.match(timestamp_line_pattern, vtt_line):
+        return True
+    else:
+        return False
+
+
+def _extract_timestamps(vtt_line: str) -> list:
+    timestamp_pattern = r"\d{2}:\d{2}:\d{2}.\d{3}"
+    timestamps: tuple = re.match(
+        rf"^({timestamp_pattern}) --> ({timestamp_pattern})", vtt_line
+    ).groups()
+    return timestamps
+
+
+def to_json(vtt_path: str) -> str:
+    return json.dumps(to_dict(vtt_path))
+
+
+def to_dict(vtt_path: str) -> dict:
+    parsed_lines = []
+    ignored_lines = []
+    parsed_line = {"start": None, "end": None, "text": None, "_raw_text": None}
+
+    with open(vtt_path, "r") as vtt_file:
+
+        for vtt_line in vtt_file:
+
+            if _is_ignored_line(vtt_line):
+                ignored_lines.append(vtt_line)
+
+            elif _is_timestamp_line(vtt_line):
+                timestamps: tuple = _extract_timestamps(vtt_line)
+                parsed_line["start"] = timestamps[0]
+                parsed_line["end"] = timestamps[1]
+
+            else:
+                parsed_line["text"] = vtt_line
+                parsed_line["_raw_text"] = vtt_line
+
+                parsed_lines.append(parsed_line)
+
+    return {"parsed_lines": parsed_lines, "ignored_lines": ignored_lines}
